9061cf9d27657d5ed84fcf415e00b35908a72a9a
Jun Liu <Julie@Juns-MacBook-Air.local>
2015-07-03 09:34:10 -0700

    merge sorted array

---
+++ binarySearch/mergeSortedArray.java
@@ -0,0 +1,28 @@
+class Solution {
+    /**
+     * @param A: sorted integer array A which has m elements, 
+     *           but size of A is m+n
+     * @param B: sorted integer array B which has n elements
+     * @return: void
+     */
+    public void mergeSortedArray(int[] A, int m, int[] B, int n) {
+        if(B == null || n == 0){
+            return;
+        }
+        
+        int i = m - 1;
+        int j = n - 1;
+        int k = m + n - 1;
+        while(i >= 0 && j >= 0){
+            if(A[i] >= B[j]){
+                A[k --] = A[i --];
+            }else{
+                A[k --] = B[j --];
+            }
+        }
+        
+        while(j >= 0){
+            A[k --] = B[j --];
+        }
+    }
+}
