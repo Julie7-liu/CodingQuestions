feeaafdbae30a5a85845a101554d86c1322aedb4
Julie7-liu <julie7.liu@gmail.com>
2015-07-07 22:30:52 -0700

    jump game add greedy solution

--- dynamicProgramming/JumpGame.java
+++ dynamicProgramming/JumpGame.java
@@ -7,30 +7,51 @@ public class Solution {
      * @param A: A list of integers
      * @return: The boolean answer
      */
+    // solution 1: dp
+    // public boolean canJump(int[] A){
+    //     if(A == null || A.length == 0){
+    //         return true;
+    //     }
+        
+    //     int n = A.length;
+    //     // initialization
+    //     boolean[] can = new boolean[n];
+    //     can[0] = true;
+    //     for(int i = 1; i < n; i++){
+    //         can[i] = false;
+    //     }
+        
+        
+    //     for(int i = 1; i < n; i++){
+    //         for(int j = 0; j < i; j ++){
+    //             if(can[j] && A[j] + j >= i){
+    //                 can[i] = true;
+    //                 break;
+    //             }
+    //         }
+    //     }
+        
+    //     return can[n - 1];
+    // }
+    
+    // solution 2: greedy 
     public boolean canJump(int[] A){
         if(A == null || A.length == 0){
             return true;
         }
         
         int n = A.length;
-        // initialization
-        boolean[] can = new boolean[n];
-        can[0] = true;
-        for(int i = 1; i < n; i++){
-            can[i] = false;
-        }
-        
-        
-        for(int i = 1; i < n; i++){
-            for(int j = 0; j < i; j ++){
-                if(can[j] && A[j] + j >= i){
-                    can[i] = true;
-                    break;
+        int max = A[0];
+        for(int i = 1; i < n; i ++){
+            if(max >= i && i + A[i] > max ){
+                max = i + A[i];
+                if(max >= n -1){
+                    return true;
                 }
             }
         }
         
-        return can[n - 1];
+        return false;
     }
 }
 
